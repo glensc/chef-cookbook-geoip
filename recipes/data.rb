0c3091afe606ff696465bf09dd9838cf6abcbbe1
Elan Ruusamäe <glen@delfi.ee>
2013-08-20 23:21:42 +0300

    fix typos

--- recipes/data.rb
+++ recipes/data.rb
@@ -6,9 +6,9 @@
 #
 
 packages = value_for_platform_family(
-  "pld" => %w{GeoIP-db-City GeoIP-db-Country GeoIP-db-IPASNum xtables-geoip}
-  "debian" => %w{geoip-database}
-  "default" => %w{geoip-database}
+  "pld" => %w{GeoIP-db-City GeoIP-db-Country GeoIP-db-IPASNum xtables-geoip},
+  "debian" => %w{geoip-database},
+  "default" => %w{geoip-database},
 )
 packages.each do |pkg|
   package pkg do
